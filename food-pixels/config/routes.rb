5033fd53f12cb592bc4acb8b8b946d74f97ef840
angelo2dot0 <angeloaramayo@gmail.com>
2015-12-12 12:43:59 -0800

    food-pixels first commit

--- food-pixels/config/routes.rb
+++ food-pixels/config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   root to: "welcome#index"
 
-  # get "/users", to: "users#index", as: "users"
+  get "/users", to: "users#index", as: "users"
   get "/users/new", to: "users#new", as: "new_user"
   post "users", to: "users#create"
   get "users/:id", to: "users#show"
