b2c6879a8f90979d8d7de6623fb029ba5882e311
angelo2dot0 <angeloaramayo@gmail.com>
2015-12-15 10:47:47 -0800

    updates

---
+++ food-pixels/app/controllers/restaurants_controller.rb
@@ -0,0 +1,27 @@
+class RestaurantsController < ApplicationController
+	def index
+		@restaurants = Restaurant.all
+		render :index
+	end
+	def create
+		@user = current_user
+		restaurant_params = params.require(:restaurant).permit(:restaurant_name, :restaurant_address, :restaurant_phone, :restaurant_website)
+		@restaurant = Restaurant.create(restaurant_params)
+		# redirect_to user_restaurants(@user.id)
+    	redirect_to "/users/#{@user.id}/restaurants"
+	end
+	def new
+		@restaurant = Restaurant.new
+		render :new
+	end
+	def edit
+	end
+	def show
+		@restaurant = Restaurant.find(params[:id])
+    	render :show
+	end
+	def update
+	end
+	def destroy
+	end
+end
